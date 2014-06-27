2991f19d730e31ec8e8b6a6d835cec57242bb5be
Anthony Swift <aswift@peartreesoftware.co.uk>
2014-06-27 12:35:46 +0100

    Improved by converting the BlockingCollection into a collection of Pulse objects rather than int. Moved the task creation into a loop to reduce the duplication.

--- AS.BlockingCollectionDemo.ConsoleUI/Program.cs
+++ AS.BlockingCollectionDemo.ConsoleUI/Program.cs
@@ -12,58 +12,45 @@ namespace AS.BlockingCollectionDemo.ConsoleUI
 
         const int MAX_BUFFER_SIZE = 500;
         const int MAX_GENERATED = 5000;
+        const int MAX_PRODUCERS = 2;
 
         static void Main()
         {
-            var blockingCollection = new BlockingCollection<int>();
+            var blockingCollection = new BlockingCollection<Pulse>();
             var count = 1;
 
             syncTimer = new Timer(new TimerCallback(TimerElapsed), blockingCollection, 0, 5000);
 
-            var p1 = Task.Factory.StartNew(() =>
-            {
-                while (count <= MAX_GENERATED)
-                {
-                    //Console.WriteLine("Producer A: " + count);
-                    blockingCollection.Add(count);
-                    Interlocked.Increment(ref count);
-                    Thread.Sleep(100);
-                }
-            });
+            Task[] producers = new Task[MAX_PRODUCERS];
 
-            var p2 = Task.Factory.StartNew(() =>
+            for (int i = 0; i < MAX_PRODUCERS; i++)
             {
-                while (count <= MAX_GENERATED)
+                var task = Task.Factory.StartNew(() =>
                 {
-                    //Console.WriteLine("Producer B: " + count);
-                    blockingCollection.Add(count);
-                    Interlocked.Increment(ref count);
-                }
-            });
+                    while (count <= MAX_GENERATED)
+                    {
+                        blockingCollection.Add(new Pulse(1, DateTime.Now));
+                        Interlocked.Increment(ref count);
+                        Thread.Sleep(100);
+                    }
+                });
 
-            var p3 = Task.Factory.StartNew(() =>
-            {
-                while (count <= MAX_GENERATED)
-                {
-                    //Console.WriteLine("Producer C: " + count);
-                    blockingCollection.Add(count);
-                    Interlocked.Increment(ref count);
-                }
-            });
+                producers[i] = task;
+            }
 
-            Task.WaitAll(p1, p2, p3);
+            Task.WaitAll(producers);
 
             Console.ReadKey(true);
         }
 
         static void TimerElapsed(object state)
         {
-            var buffer = new List<int>(MAX_BUFFER_SIZE);
+            var buffer = new List<Pulse>(MAX_BUFFER_SIZE);
 
-            int value = 0;
-            while (((BlockingCollection<int>)state).TryTake(out value, 0))
+            Pulse pulse = null;
+            while (((BlockingCollection<Pulse>)state).TryTake(out pulse, 0))
             {
-                buffer.Add(value);
+                buffer.Add(pulse);
 
                 if (buffer.Count == MAX_BUFFER_SIZE)
                 {
@@ -75,10 +62,10 @@ namespace AS.BlockingCollectionDemo.ConsoleUI
             buffer.Clear();
         }
 
-        static void ProcessItems(List<int> buffer)
+        static void ProcessItems(List<Pulse> buffer)
         {
             // TODO: Process items accordingly.
-            Console.WriteLine("Worker: Processing {0} items", buffer.Count);
+            Console.WriteLine("Worker: Processing {0} pulses", buffer.Count);
         }
     }
 }
---
+++ AS.BlockingCollectionDemo.ConsoleUI/Pulse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS.BlockingCollectionDemo.ConsoleUI
+{
+    public class Pulse
+    {
+        public int Pin { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public Pulse(int pin, DateTime timestamp)
+        {
+            Pin = pin;
+            Timestamp = timestamp;
+        }
+    }
+}
